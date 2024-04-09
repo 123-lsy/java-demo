5ee4eebb63490e34ac70c93f6308f249f116c615
shiying.li <2047564271@qq.com>
2024-04-09 16:56:13 +0800

    springboot聚合nacos

---
+++ demo-provider/src/main/java/com/demo/provider/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.demo.provider.controller;
+
+import com.demo.common.util.CommonResponse;
+import com.demo.provider.controller.dto.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("demo/provider")
+public class UserController {
+
+    @RequestMapping("/getUser")
+    public CommonResponse<Object> getUser(){
+        User user = new User();
+        user.setName("lili");
+        user.setPhone(123124324);
+        return CommonResponse.success(user, "success");
+    }
+}
---
+++ demo-provider/src/main/java/com/demo/provider/controller/dto/User.java
@@ -0,0 +1,9 @@
+package com.demo.provider.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private Integer phone;
+}
